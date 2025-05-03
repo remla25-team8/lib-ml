a8168bcf997341f359fd53729620afe62b141702
yongchenghuang <Y.Huang-51@student.tudelft.nl>
2025-05-03 17:11:30 +0200

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="remla25-team8-lib-ml",
-    version="0.1.4",
+    version="0.1.5",
     author="remla25-team8",
     description="Restaurant Sentiment Analysis",
     long_description=open("README.md").read(),
